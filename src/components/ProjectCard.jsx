d23f245c0b4e03cfdd1458e5272a4ed55db0368a
dscelestial <celestial.danielstephen@gmail.com>
2024-06-23 18:16:32 +0800

    Project section

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,11 +3,16 @@ import React from 'react'
 const ProjectCard = ({ imgUrl, title, description }) => {
     return (
         <div>
-            <div className='h-52 md:h-72' style={{ background: `url( ${imgUrl} )`, backgroundSize: "cover"}}>
-            </div>
-            <div className='text-white'>
-                <h5>{title}</h5>
-                <p>{description}</p>
+            <div
+                className="h-52 md:h-72 rounded-xl relative group bg-center bg-cover bg-no-repeat"
+                style={{ backgroundImage: `url(${imgUrl})` }}
+                >
+                <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
+                </div>
+                </div>
+            <div className="text-white rounded-b-xl mt-3 bg-[#181818]py-6 px-4">
+                <h5 className="text-xl font-semibold mb-2">{title}</h5>
+                <p className="text-[#ADB7BE]">{description}</p>
             </div>
         </div>
     )
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,21 +6,42 @@ const projectsData = [
         id: 1,
         title: "React Portfolio Website",
         description: "My Portolfio",
-        image: "/src/assets/projectimages/portfolio.png",
+        image: "/src/assets/projectimages/col.png",
         tag: ["All", "Web"],
     },
     {
         id: 2,
         title: "Laravel Twitter Clone",
         description: "Laravel",
-        image: "/src/assets/projectimages/portfolio.png",
+        image: "/src/assets/projectimages/1.png",
         tag: ["All", "Web"],
     },
     {
         id: 3,
         title: "Chess",
         description: "Java OOP",
-        image: "/src/assets/projectimages/portfolio.png",
+        image: "/src/assets/projectimages/2.png",
+        tag: ["All", "Web"],
+    },
+    {
+        id: 4,
+        title: "React Portfolio Website",
+        description: "My Portolfio",
+        image: "/src/assets/projectimages/1.png",
+        tag: ["All", "Web"],
+    },
+    {
+        id: 5,
+        title: "Laravel Twitter Clone",
+        description: "Laravel",
+        image: "/src/assets/projectimages/2.png",
+        tag: ["All", "Web"],
+    },
+    {
+        id: 6,
+        title: "Chess",
+        description: "Java OOP",
+        image: "/src/assets/projectimages/col.png",
         tag: ["All", "Web"],
     },
 ];
@@ -28,7 +49,7 @@ const projectsData = [
 const Projects = () => {
     return (
         <div>
-            <h2>My Project</h2>
+            <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">Projects</h2>
             <div className='grid md:grid-cols-3 gap-8 md:gap-12'>
                 {projectsData.map( (project) => (
                     <ProjectCard
