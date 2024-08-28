66b3d301fd1d556ab6cefc9a44f70fbfc588be73
Markrey Manabat <markreym0@gmail.com>
2024-08-28 11:28:13 +0800

    Experience Added

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -2,8 +2,119 @@ import React from 'react';
 
 const Experience = () => {
     return (
-        <section className='my-16 mx-0'>
-            <h5>My Experience</h5>
+        <section className='flexe justify-center'>
+            <h1>My Experience</h1>
+            <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
+                <li>
+                    <div className="timeline-middle">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            className="h-5 w-5">
+                            <path
+                            fillRule="evenodd"
+                            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                            clipRule="evenodd" />
+                        </svg>
+                    </div>
+                    <div className="timeline-start mb-10 md:text-end">
+                        <time className="font-mono italic">1984</time>
+                        <div className="text-lg font-black">First Macintosh computer</div>
+                        The Apple Macintosh—later rebranded as the Macintosh 128K—is the original Apple Macintosh
+                        personal computer. It played a pivotal role in establishing desktop publishing as a general
+                        office function. The motherboard, a 9 in (23 cm) CRT monitor, and a floppy drive were housed
+                        in a beige case with integrated carrying handle; it came with a keyboard and single-button
+                        mouse.
+                    </div>
+                    <hr />
+                </li>
+                <li>
+                    <hr />
+                    <div className="timeline-middle">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            className="h-5 w-5">
+                            <path
+                            fillRule="evenodd"
+                            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                            clipRule="evenodd" />
+                        </svg>
+                    </div>
+                    <div className="timeline-end mb-10">
+                        <time className="font-mono italic">2024</time>
+                        <div className="text-lg font-black">Software Developer</div>
+                        Xtreme Offshore Outsourcing Inc.
+                        <p>Tech Learned: PHP, Wordpress, Javascript, CSS, SCSS, Git, Github</p>
+                    </div>
+                    <hr />
+                </li>
+                <li>
+                    <hr />
+                    <div className="timeline-middle">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            className="h-5 w-5">
+                            <path
+                            fillRule="evenodd"
+                            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                            clipRule="evenodd" />
+                        </svg>
+                    </div>
+                    <div className="timeline-start mb-10 md:text-end">
+                        <time className="font-mono italic">2023</time>
+                        <div className="text-lg font-black">Web Developer / Designer</div>
+                        HR Management Business Solution Inc. 
+                        <p>Tech Learned: PHP, Wordpress, MySql, Github</p>
+                    </div>
+                    <hr />
+                </li>
+                <li>
+                    <hr />
+                    <div className="timeline-middle">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            className="h-5 w-5">
+                            <path
+                            fillRule="evenodd"
+                            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                            clipRule="evenodd" />
+                        </svg>
+                    </div>
+                    <div className="timeline-end mb-10">
+                        <time className="font-mono italic">2019 - 2023</time>
+                        <div className="text-lg font-black">Bachelor Degree - Information Technology</div>
+                        Polytechnic University of the Philippines - Taguig Branch
+                    </div>
+                    <hr />
+                </li>
+                <li>
+                    <hr />
+                    <div className="timeline-middle">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            className="h-5 w-5">
+                            <path
+                            fillRule="evenodd"
+                            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                            clipRule="evenodd" />
+                        </svg>
+                    </div>
+                    <div className="timeline-start mb-10 md:text-end">
+                        <time className="font-mono italic">2017 - 2019</time>
+                        <div className="text-lg font-black">Senior High School - Information Communication Technology</div>
+                        Senator Renato Companero Cayetano Memorial Science High School
+                    </div>
+                </li>
+            </ul>
         </section>
     )
 }
