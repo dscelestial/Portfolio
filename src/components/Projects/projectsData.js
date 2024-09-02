7e0993d33378e71022de8b1722616f4ea991b0b9
dscelestial <celestial.danielstephen@gmail.com>
2024-09-02 10:13:56 +0800

    Last image fix

--- src/components/Projects/projectsData.js
+++ src/components/Projects/projectsData.js
@@ -7,22 +7,22 @@ const projectsData = [
         id: 1,
         title: "My Portfolio",
         description: "My Portfolio created using ReactJS, TailWindCSS and DaisyUI",
-        image: "/src/assets/images/portfolio.png",
-        githubUrl: myportfolio,
+        image: myportfolio,
+        githubUrl: "https://github.com/dscelestial/Portfolio",
     },
     {
         id: 2,
         title: "Java Calculator",
         description: "Created a simple calculator using Java Swing and Java Awt",
-        image: "/src/assets/images/JavaCalculator.png",
-        githubUrl: javacalculator,
+        image: javacalculator,
+        githubUrl: "https://github.com/dscelestial/JavaCalculator",
     },
     {
         id: 3,
         title: "ReaderWorld",
         description: "My first project, tutorial by Traversy Media",
-        image: "/src/assets/images/BookReader.png",
-        githubUrl: readerworld,
+        image: readerworld,
+        githubUrl: "https://github.com/dscelestial/laragigs",
     },
 ];
 
